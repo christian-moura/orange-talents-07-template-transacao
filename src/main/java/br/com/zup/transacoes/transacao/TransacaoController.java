a82f44035d565ace5d110fb799d0952cdabf6598
Christian Moura <christian.moura@zup.com.br>
2021-09-07 15:59:49 -0300

    autenticação e autorização

--- src/main/java/br/com/zup/transacoes/transacao/TransacaoController.java
+++ src/main/java/br/com/zup/transacoes/transacao/TransacaoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/proposta")
+@RequestMapping("/api/transacao")
 public class TransacaoController {
 
     private TransacaoRepository transacaoRepository;
